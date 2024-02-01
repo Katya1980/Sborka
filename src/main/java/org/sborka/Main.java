7009df7672659f78aaf0f0da15d63191c055aed3
Lenovo <nikamotors2020@mail.ru>
2024-02-01 15:34:33 +0300

    Test

--- src/main/java/org/sborka/Main.java
+++ src/main/java/org/sborka/Main.java
@@ -3,5 +3,7 @@ package org.sborka;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello maven!");
+
+        System.out.println("Hello world!");
     }
 }
